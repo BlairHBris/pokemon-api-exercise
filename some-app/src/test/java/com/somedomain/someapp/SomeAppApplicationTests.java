57b7f5d8da6f159885597e22e31d144b8dafbccc
Blair Bristow <blairhbristow@gmail.com>
2022-05-31 14:08:42 -0400

    Failed build

---
+++ some-app/src/test/java/com/somedomain/someapp/SomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somedomain.someapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
